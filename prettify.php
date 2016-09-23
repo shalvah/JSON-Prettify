3d11780665bd9bef6a340ef20859aa39feeb56f5
Shalvah <diakon.ng@gmail.com>
2016-09-23 20:10:40 +0100

    Refactor

--- prettify.php
+++ prettify.php
@@ -6,26 +6,26 @@
 <body>
 
 	<?php
-	if (isset($_POST['prettify-submit'])) 
-	{
-		if (isset($_FILES['json-file'])) 
+	if (isset($_POST['prettify-submit'])) :
+		if (isset($_FILES['json-file'])) :
 			$jsonString=file_get_contents($_FILES['json-file']['tmp_name']);
-		else{
-			$jsonString=$_POST['json'];
-		}
+		else :
+			$jsonString=$_POST['json-text'];
+		endif;
+
 		$prettified=prettify($jsonString);
 		$file=fopen("prettified.json", "w");
 		fwrite($file, html_entity_decode($prettified));
 		fclose($file);
 		?>
 		<h3>Formatted string (<a href="prettified.json" download>Download</a> | <a href="#input-form" >Prettify another string</a>):</h3><br>
-		<?php echo nl2br($prettified); ?><br><br>"
+		<?= nl2br($prettified); ?><br><br>"
 		<?php
-	}
+	endif;
 
 	function prettify($json)
 	{
-		$curlyBracePointer=0;
+		$depth=0;
 		$inQuote=false;
 		$pretty=$json;
 
@@ -35,18 +35,18 @@
 		{
 			if($pretty[$i]=='{' && !$inQuote)
 			{
-				$curlyBracePointer++;
-				$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
+				$depth++;
+				$tabs=($depth<=0) ? '' : str_repeat('&nbsp;', 4*$depth);
 				$replacement="{\n$tabs";
 				$pretty=substr_replace($pretty, $replacement, $i, 1);
 			}
 			elseif($pretty[$i]=='}' && !$inQuote)
 			{
-				$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
+				$tabs=($depth<=0) ? '' : str_repeat('&nbsp;', 4*$depth);
 				$replacement="\n$tabs}";
 				$pretty=substr_replace($pretty, $replacement, $i, 1);
 				$i=$i+strlen($replacement)-1;
-				$curlyBracePointer--;
+				$depth--;
 			}
 			elseif($pretty[$i]=="\"" && ($i==0 || $pretty[$i-1]!="\\"))
 			{
@@ -55,10 +55,10 @@
 			elseif(($pretty[$i]==',') && !$inQuote )
 			{
 				if ($pretty[$i+1]=="{") {
-					$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*($curlyBracePointer+1));
+					$tabs=($depth<=0) ? '' : str_repeat('&nbsp;', 4*($depth+1));
 				}
 				elseif (($pretty[$i+1]=="\"")) {
-					$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
+					$tabs=($depth<=0) ? '' : str_repeat('&nbsp;', 4*$depth);
 				}
 				$replacement=",\n$tabs";
 				$pretty=substr_replace($pretty, $replacement, $i, 1);
@@ -82,7 +82,7 @@
 
 	<h2>Enter JSON string:</h2>
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" id="input-form">
-		<textarea name="json" required></textarea>
+		<textarea name="json-text" required></textarea>
 		<div><input type="submit" name="prettify-submit" value="Prettify"></div>
 	</form>
 	<h2>Or upload a JSON file:</h2>
