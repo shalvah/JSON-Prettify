e0a0526d9b19cb3bc06442795189570b41d7d069
Shalvah <diakon.ng@gmail.com>
2016-09-23 01:50:01 +0100

    Add upload and download file options. v1.1

--- prettify.php
+++ prettify.php
@@ -1,79 +1,95 @@
 <!DOCTYPE html>
 <html>
-	<head>
+<head>
 	<title>JSON-Prettify</title>
-	</head>
-	<body>
+</head>
+<body>
 
 	<?php
-if (isset($_POST['json'])) 
-{
-	echo "Formatted string:<br>".prettify($_POST['json'])."<br><br>";
-}
-
-function prettify($json)
-{
-	$curlyBracePointer=0;
-	$inQuote=false;
-	$pretty=$json;
-
-	$i=0;
-
-	while ($i<strlen($pretty))  
+	if (isset($_POST['prettify-submit'])) 
 	{
-		if($pretty[$i]=='{' && !$inQuote)
-		{
-			$curlyBracePointer++;
-			$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
-			$replacement="{<br>$tabs";
-			$pretty=substr_replace($pretty, $replacement, $i, 1);
+		if (isset($_FILES['json-file'])) 
+			$jsonString=file_get_contents($_FILES['json-file']['tmp_name']);
+		else{
+			$jsonString=$_POST['json'];
 		}
-		elseif($pretty[$i]=='}' && !$inQuote)
-		{
-			$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
-			$replacement="<br>$tabs}";
-			$pretty=substr_replace($pretty, $replacement, $i, 1);
-			$i=$i+strlen($replacement)-1;
-			$curlyBracePointer--;
-		}
-		elseif($pretty[$i]=="\"" && ($i==0 || $pretty[$i-1]!="\\"))
-		{
-			toggleInQuote();
-		}
-		elseif(($pretty[$i]==',') && !$inQuote )
-		{
-			if ($pretty[$i+1]=="{") {
-				$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*($curlyBracePointer+1));
-			}
-			elseif (($pretty[$i+1]=="\"")) {
-				$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
-			}
-			$replacement=",<br>$tabs";
-			$pretty=substr_replace($pretty, $replacement, $i, 1);
-			$i=$i+strlen($replacement)-1;
-		}
-		$i++;
+		$prettified=prettify($jsonString);
+		$file=fopen("prettified.json", "w");
+		fwrite($file, html_entity_decode($prettified));
+		fclose($file);
+		?>
+		<h3>Formatted string (<a href="prettified.json" download>Download</a> | <a href="#input-form" >Prettify another string</a>):</h3><br>
+		<?php echo nl2br($prettified); ?><br><br>"
+		<?php
 	}
-	return $pretty;
-}
 
-function toggleInQuote()
-{
-	global $inQuote;
-	if($inQuote)
+	function prettify($json)
+	{
+		$curlyBracePointer=0;
 		$inQuote=false;
-	else
-		$inQuote=true;	
-}
+		$pretty=$json;
 
-?>
+		$i=0;
 
-<h2>Enter JSON string:</h2>
-		<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
-			<textarea name="json" required></textarea>
-			<input type="submit" name="prettify" value="Prettify">
-		</form>
-	</body>
+		while ($i<strlen($pretty))  
+		{
+			if($pretty[$i]=='{' && !$inQuote)
+			{
+				$curlyBracePointer++;
+				$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
+				$replacement="{\n$tabs";
+				$pretty=substr_replace($pretty, $replacement, $i, 1);
+			}
+			elseif($pretty[$i]=='}' && !$inQuote)
+			{
+				$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
+				$replacement="\n$tabs}";
+				$pretty=substr_replace($pretty, $replacement, $i, 1);
+				$i=$i+strlen($replacement)-1;
+				$curlyBracePointer--;
+			}
+			elseif($pretty[$i]=="\"" && ($i==0 || $pretty[$i-1]!="\\"))
+			{
+				toggleInQuote();
+			}
+			elseif(($pretty[$i]==',') && !$inQuote )
+			{
+				if ($pretty[$i+1]=="{") {
+					$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*($curlyBracePointer+1));
+				}
+				elseif (($pretty[$i+1]=="\"")) {
+					$tabs=($curlyBracePointer<=0) ? '' : str_repeat('&nbsp;', 4*$curlyBracePointer);
+				}
+				$replacement=",\n$tabs";
+				$pretty=substr_replace($pretty, $replacement, $i, 1);
+				$i=$i+strlen($replacement)-1;
+			}
+			$i++;
+		}
+		return $pretty;
+	}
+
+	function toggleInQuote()
+	{
+		global $inQuote;
+		if($inQuote)
+			$inQuote=false;
+		else
+			$inQuote=true;	
+	}
+
+	?>
+
+	<h2>Enter JSON string:</h2>
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" id="input-form">
+		<textarea name="json" required></textarea>
+		<div><input type="submit" name="prettify-submit" value="Prettify"></div>
+	</form>
+	<h2>Or upload a JSON file:</h2>
+	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" enctype="multipart/form-data">
+	<input type="file" name="json-file" required><div><input type="submit" name="prettify-submit" value="Prettify"></div>
+	</form>
+</body>
 </html>
 
 
